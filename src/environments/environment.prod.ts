382dea16759243beaee5c1f2f49892fc3eda6772
darge <mattia.darge@gmail.com>
2024-11-29 19:01:59 +0100

    build: fix build

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  clientId: '141368',
-  clientSecret: '41e02252d605e335ad44e434f0b89693820bd4f3',
-  redirectUri: 'https://strava-heatmap-gold.vercel.app/auth-callback'
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   clientId: '141368',
   clientSecret: '41e02252d605e335ad44e434f0b89693820bd4f3',
-  redirectUri: 'http://localhost:4200/auth-callback'
+  redirectUri: 'https://strava-heatmap-gold.vercel.app/auth-callback'
 };
